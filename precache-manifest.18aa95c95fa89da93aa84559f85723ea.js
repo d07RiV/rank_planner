0dd36c0ec7e98eae5c20046e3edf9d12e63fdf76
d07riv <d07.riv@gmail.com>
2020-05-12 22:35:50 +0300

    Updates

---
+++ precache-manifest.18aa95c95fa89da93aa84559f85723ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c30f45342b389fd2353d022e32f704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "219ef65e65635f350df0",
+    "url": "/static/css/main.c9e36c4d.chunk.css"
+  },
+  {
+    "revision": "d9f5f382af13d41cd53a",
+    "url": "/static/js/2.793a6082.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.793a6082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219ef65e65635f350df0",
+    "url": "/static/js/main.52055727.chunk.js"
+  },
+  {
+    "revision": "8a936c6e9c0e116baf54",
+    "url": "/static/js/runtime-main.4b8de40e.js"
+  }
+]);
